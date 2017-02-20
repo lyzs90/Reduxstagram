75712331cecb68edc6076c6fed30de250130cdfd
lzys90 <leongyzs@gmail.com>
2017-02-21 02:05:06 +0800

    Add Sentry logging

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,6 +4,10 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store.js';
 
+// Sentry
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
 // Import css
 import css from './styles/style.styl';
 
@@ -11,6 +15,8 @@ import css from './styles/style.styl';
 import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
+
+Raven.config(sentry_url).install();
 
 const router = (
   <Provider store={store}>
